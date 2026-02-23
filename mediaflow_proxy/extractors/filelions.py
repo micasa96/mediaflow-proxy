ab2312a3d518bf9cdb8cd3af2e548032a441311c
micasa96 <casamendez96@gmail.com>
2026-02-22 23:07:37 -0600

    correction in headers filelions cdn tiktok

--- mediaflow_proxy/extractors/filelions.py
+++ mediaflow_proxy/extractors/filelions.py
@@ -10,16 +10,27 @@ class FileLionsExtractor(BaseExtractor):
         self.mediaflow_endpoint = "hls_manifest_proxy"
 
     async def extract(self, url: str, **kwargs) -> Dict[str, Any]:
-        headers = {}
-        patterns = [  # See https://github.com/Gujal00/ResolveURL/blob/master/script.module.resolveurl/lib/resolveurl/plugins/filelions.py
-            r"""sources:\s*\[{file:\s*["'](?P<url>[^"']+)""",
-            r"""["']hls4["']:\s*["'](?P<url>[^"']+)""",
-            r"""["']hls2["']:\s*["'](?P<url>[^"']+)""",
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 13; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.92 Mobile Safari/537.36",
+            "Accept": "*/*",
+            "Referer": url,
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
+        }
+        
+        patterns = [
+            r"""["'](?:hls\d*|source|file)["']\s*:\s*["']([^"']*(?:\.m3u8|\.hls)[^"']*)["']""",
+            r"""sources:\s*\[{file:\s*["']([^"']+)["']""",
+            r"""["']file["']\s*:\s*["']([^"']*(?:\.m3u8|\.hls)[^"']*)["']""",
+            r"""(?:hls|source)_url\s*:\s*["']([^"']*(?:\.m3u8|\.hls)[^"']*)["']""",
         ]
 
         final_url = await eval_solver(self, url, headers, patterns)
 
+    
         self.base_headers["referer"] = url
+        
         return {
             "destination_url": final_url,
             "request_headers": self.base_headers,
